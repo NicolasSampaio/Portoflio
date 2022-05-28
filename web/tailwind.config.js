de040b5d73869a623fa729fa3a7686858f3d94fb
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2022-05-28 04:36:59 -0300

    feat: new fonts

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["./src/**/*.{html,js,tsx}"],
   theme: {
     extend: {
+      //font-family: 'PT Sans', sans-serif;
+      fontFamily: {
+        ptsans: ["PT Sans", "sans-serif"],
+        avenirBlack: ["Avenir Black", "cursive"],
+        avenirBook: ["Avenir Book", "cursive"],
+      },
       backgroundImage: {
         texture: "url('./assets/BG.svg')",
       },
