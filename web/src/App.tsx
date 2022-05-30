024141cd79c7b5badd6bc7161cc4bb7f7e70ad5d
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2022-05-30 13:56:01 -0300

    feat: new assets

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,16 @@
 import { Header } from "./components/Header";
-import { Main } from "./components/Main";
 
 import { Outlet, Link } from "react-router-dom";
+import { Footer } from "./components/Footer";
 
 export function App() {
   return (
-    <div className="bg-texture h-screen w-screen">
+    <div className="flex flex-col justify-between bg-texture h-screen w-screen">
       <Header />
 
       <Outlet />
+
+      <Footer />
     </div>
   );
 }
