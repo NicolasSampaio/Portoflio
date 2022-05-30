df683674b2c832e93e43d213eaba2262d79e8a0f
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2022-05-30 13:55:04 -0300

    feat: footer component

---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import { EnvelopeSimple } from "phosphor-react";
+
+export function Footer() {
+  return (
+    <footer className="flex justify-between content-center mx-8 mb-8">
+      <a href="mailto:nicolas.pereira.sampaio16@gmail.com">
+        <button>
+          <EnvelopeSimple
+            size={52}
+            color="#F04848"
+            // className="absolute bottom-11 left-8"
+          />
+        </button>
+      </a>
+
+      <span className="text-1.5xs text-lincoliGrey">
+        All rights, lefts and nelsons reserved. 2022
+      </span>
+    </footer>
+  );
+}
