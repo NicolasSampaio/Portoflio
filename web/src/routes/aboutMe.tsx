5548a34657383c2a9e23fb3aeb73727fbe23eea5
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2022-05-30 13:55:55 -0300

    feat: aboutme component

--- web/src/routes/aboutMe.tsx
+++ web/src/routes/aboutMe.tsx
@@ -1,3 +1,55 @@
+import { MyRecentWorkButton } from "../components/MyRecentWorkButton";
+
+import githubImg from "../assets/github.svg";
+import instagramImg from "../assets/instagram.svg";
+import linkedinImg from "../assets/linkedin.svg";
+import perfilImg from "../assets/perfil.png";
+
 export function AboutMe() {
-  return <h1 className="text-5xl text-white">About Me</h1>;
+  return (
+    <main className="flex">
+      <h2
+        className="flex items-center justify-center w-40 -rotate-90
+      text-7.5xl whitespace-nowrap text-white"
+      >
+        about me
+      </h2>
+
+      <span className="absolute w-96 h-2 border border-lincoliRed bg-lincoliRed rotate-90 rounded top-[25rem] -left-[3.5rem]"></span>
+      <div className="max-w-3xl ml-24">
+        <img src={perfilImg} alt="Foto de Perfil" className="w-80 h-80" />
+        <p className="text-1xl text-white leading-9">
+          Be better than yesterday.
+          <br /> Majoring in Information Systems. I love the feeling of learning
+          something new, and especially of sharing.
+          <br /> I like to see a macro process and be able to see the difference
+          I made there, and to see a micro process and seek more and more
+          improvements.
+          <br /> Softskills enthusiast.
+        </p>
+        <h3 className="text-2xl leading-10 text-lincoliRed">
+          I have experience with
+        </h3>
+        <p className="text-1xl leading-9 text-white">
+          <strong>Backend</strong>: .NET, PostgreSQL, SQL Server, MySQL
+          <br />
+          <strong>Frontend</strong>: Html, Css, Javascript, Typescript, Vite,
+          Tailwind, React, Angular
+        </p>
+        <MyRecentWorkButton styleInfo="mt-2" />
+      </div>
+
+      <div className="flex flex-col grow justify-center content-center ml-24 mr-40">
+        <h3 className="text-center text-3xl leading-10 text-white">
+          Get in Touch
+        </h3>
+        <span className="w-[11rem] self-center mt-1 mb-8 border border-white"></span>
+        <div className="flex justify-between">
+          <img src={githubImg} alt="Github Icon" className="w-20 h-20" />
+          <img src={instagramImg} alt="Github Icon" className="w-20 h-20" />
+          <img src={linkedinImg} alt="Github Icon" className="w-20 h-20" />
+        </div>
+      </div>
+    </main>
+  );
 }
