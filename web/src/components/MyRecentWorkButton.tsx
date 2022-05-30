f280c67ed12c3b5f5ad9072369b4459bd0ab05b1
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2022-05-30 13:55:28 -0300

    feat: button component

---
+++ web/src/components/MyRecentWorkButton.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+interface MyRecentWorkButtonProps {
+  styleInfo?: string;
+}
+
+export function MyRecentWorkButton({ styleInfo }: MyRecentWorkButtonProps) {
+  return (
+    <Link to="/portfolio">
+      <button
+        className={`relative border rounded-xl border-white py-7 px-16 text-2.1xl text-white
+        after:absolute after:content-[url('./assets/ArrowRight.svg')] after:h-12 after:w-20 after:top-5 after:-right-12 ${styleInfo}`}
+      >
+        My Recent Work
+      </button>
+    </Link>
+  );
+}
