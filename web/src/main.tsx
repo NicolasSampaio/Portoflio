4732c024b22f6ad6a6912aa6bb6cebbbb7fd7da4
Nicolas Sampaio <nicolas.pereira.sampaio16@gmail.com>
2022-06-04 11:05:03 -0300

    fix: trying to allow access page directly by url²

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -19,12 +19,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<App />}>
             <Route index element={<Home />} />
 
-            <Route path="portfolio" element={<Portfolio />}>
-              <Route index element={<Portfolio />} />
-            </Route>
-            <Route path="aboutme" element={<AboutMe />}>
-              <Route index element={<AboutMe />} />
-            </Route>
+            <Route path="portfolio" element={<Portfolio />}></Route>
+            <Route path="aboutme" element={<AboutMe />}></Route>
           </Route>
 
           <Route
